31fed99d7def90af042af4401abd2f557a0dc8e6
Rob LeGrand <roblegrand98@gmail.com>
2026-08-05 16:59:26 -0500

    Modernize and simplify creation of newVotePoint array for simplex votespace in createRandomVotePoint function

--- script.js
+++ script.js
@@ -333,15 +333,15 @@ document.addEventListener('DOMContentLoaded', function () {
    const createRandomVotePoint = () => projectVotePointToSpace(
       simplexRadio.checked
       ? (function () {
-         const newVotePoint = Array.from({length: numDims});
+         let newVotePoint;
          do {
-            newVotePoint[0] = 1;
-            newVotePoint.forEach(function (ignore, whichDim) {
-               if (whichDim > 0) {
-                  newVotePoint[whichDim] = createRandomVoteDim();
-                  newVotePoint[0] -= newVotePoint[whichDim];
-               }
-            });
+            newVotePoint = Array.from(
+               {length: numDims},
+               createRandomVoteDim
+            );
+            newVotePoint[0] = 1 + newVotePoint[0] - newVotePoint.reduce(
+               (x, y) => x + y
+            );
          } while (newVotePoint[0] < 0);
          return newVotePoint;
       }())
